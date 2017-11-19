cd2edf960d8b8987f307739cd16572682686a9f9
Kuba Maruszczyk <kuba_maruszczyk@hotmail.co.uk>
2017-11-19 17:58:22 +0000

    texture mode = GL_NEAREST

--- HFRPlayer/Renderer.cpp
+++ HFRPlayer/Renderer.cpp
@@ -57,7 +57,7 @@ bool Renderer::Start(int w, int h, std::string title, bool fullScreen)
 	//Set the GLFW window creation hints - these are optional  
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version  
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version  
-	glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing  
+	//glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing  
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//Create a window and create its OpenGL context  
