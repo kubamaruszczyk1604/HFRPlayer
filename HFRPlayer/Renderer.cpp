69e51c480cab55a38329adf78933f7eb465bc77c
km722 <km722@KRYPTON>
2017-11-21 19:05:01 +0000

    Clean up

--- HFRPlayer/Renderer.cpp
+++ HFRPlayer/Renderer.cpp
@@ -90,19 +90,19 @@ bool Renderer::Init(int w, int h, std::string title, bool fullScreen)
 	{
 		return false;
 	}
-	//Set the GLFW window creation hints - these are optional  
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version  
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version  
-	//glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing  
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
+	//glfwWindowHint(GLFW_SAMPLES, 4); 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//Create a window and create its OpenGL context  
+	
 	if(fullScreen)
 	s_GLWindow = glfwCreateWindow(w, h, title.c_str(), glfwGetPrimaryMonitor(), NULL);
 	else 
 		s_GLWindow = glfwCreateWindow(w, h, title.c_str(), NULL, NULL);
 
-	//If the window couldn't be created  
+
 	if (!s_GLWindow)
 	{
 		fprintf(stderr, "Failed to open GLFW window.\n");
@@ -110,7 +110,7 @@ bool Renderer::Init(int w, int h, std::string title, bool fullScreen)
 		exit(EXIT_FAILURE);
 	}
 
-	//This function makes the context of the specified window current on the calling thread.   
+	   
 	glfwMakeContextCurrent(s_GLWindow);
 
 	//Initialize GLEW  
